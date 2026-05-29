35b339babb5e1cb32c65aece334e12fd3bc5e171
Kristex95 <kristex95@gmail.com>
2026-05-29 18:44:17 +0300

    laravel telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,15 +1,10 @@
 <?php
 
-use App\Modules\Accounts\Providers\AccountsServiceProvider;
-use App\Modules\Auth\Providers\AuthServiceProvider;
-use App\Modules\Balances\Providers\BalancesServiceProvider;
-use App\Modules\Orders\Providers\OrdersServiceProvider;
-use App\Providers\AppServiceProvider;
-
 return [
-    AppServiceProvider::class,
-    AuthServiceProvider::class,
-    AccountsServiceProvider::class,
-    BalancesServiceProvider::class,
-    OrdersServiceProvider::class,
+    App\Modules\Accounts\Providers\AccountsServiceProvider::class,
+    App\Modules\Auth\Providers\AuthServiceProvider::class,
+    App\Modules\Balances\Providers\BalancesServiceProvider::class,
+    App\Modules\Orders\Providers\OrdersServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
